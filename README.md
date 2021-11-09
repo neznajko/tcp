133d047f9898ce7ae0813c546d2d8643d558232d
Dimitar Panayotov <neznajko@yahoo.com>
2021-11-09 17:43:10 +0200

    pedantic

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Socket programming homework
+# Socket programming homework
+## Winkle
+#### listing.c, List Network Interfaces
+```C++
++----------------+-----------------+
+| <ifaddr.h>     | <bits/socket.h> |
++----------------+-----------------+
+| struct ifaddrs | struct sockaddr |
+| getifaddrs()   |                 |
+| freeifaddr()   |                 |
++----------------+----+------------+--+
+| <netinet/in.h>      | <netdb.h>     |
++---------------------+---------------+
+| struct sockaddr_in  | getnameinfo() |
+| struct sockaddr_in6 |               |
++---------------------+---------------+
+Usage: gcc -pedantic listing.c && ./a.out
+```
--- Winkle/listing.c
+++ Winkle/listing.c
@@ -1,5 +1,44 @@
-#include <ifaddrs.h> // network interface addresses
+                      ////
+////////////////////////////////////////////////
+#include <ifaddrs.h> // interface addresses
+#include <netdb.h>
+#include <stdio.h>
 int main()
 {
     struct ifaddrs *ifaddrs;
+    if( getifaddrs( &ifaddrs) == -1) {
+        puts( "getifaddrs: Boom");
+        return 1;
+    }
+    for( struct ifaddrs *p = ifaddrs;
+         p != NULL; p = p->ifa_next) {
+        socklen_t len;
+        int version;
+        switch( p->ifa_addr->sa_family) {
+            case AF_INET: // 2
+            {
+                len = sizeof( struct sockaddr_in);
+                version = 4;
+                break;
+            }
+            case AF_INET6: // 10
+            {
+                len = sizeof( struct sockaddr_in6);
+                version = 6;
+                break;
+            }
+            default: continue;
+        }
+        char host[64];
+        getnameinfo( p->ifa_addr, len,
+                     host, sizeof host,
+                     0, 0, NI_NUMERICHOST);
+        printf( "%-20s IPv%d %s\n",
+                p->ifa_name, version, host);
+    }
+    freeifaddrs( ifaddrs);
 }
+////////////////////////////////////////////////
+// log:
+////////////////////////////////////////////////
+    ////                                ////
