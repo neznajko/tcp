184211acfcc3a57581caf3da0bd43344ef5f4f46
4i4mitko <we4er@abv.bg>
2023-06-13 23:25:05 +0300

    🍧

--- Winkle/winlist.cpp
+++ Winkle/winlist.cpp
@@ -1,63 +1,78 @@
-# include <WinSock2.h>
-# include <iphlpapi.h>
-# include <ws2tcpip.h>
-# include <iostream>
-# pragma comment( lib, "ws2_32.lib")
+////////////////////////////////////////////////////////////////
+# include <WinSock2.h> // WSADATA, WSAStartup, WSACleanup
+# include <iphlpapi.h> // PIP_ADAPTER_ADDRESSES ..
+# include <ws2tcpip.h> // getnameinfo
+# pragma comment( lib, "ws2_32.lib" )
 # pragma comment( lib, "iphlpapi.lib")
-////////////////////////////////////////////////////////////////////////
-// clean the mess
-# define au_revoir( EXIT_CODE) {\
-	WSACleanup();\
-	free( bufr);\
-	return( EXIT_CODE);\
+# include <iostream>
+////////////////////////////////////////////////////////////////
+using std::endl;
+////////////////////////////////////////////////////////////////
+std::ostream& operator<<( std::ostream& os, WSADATA wsa_data ){
+    return os << wsa_data.szDescription;
 }
-int main() {
-	// Initialize winsock library.
-	WSADATA d; // WSA = WinSock API
-	if( WSAStartup( MAKEWORD( 2, 2), &d)) {
-		std::cout << "Hello World!\n";
-		return 1;
-	}
-	std::cout << "Ok.\n";
-	// get adapters( adapta)
-	DWORD adpt_size = 16384; // 16kB( 2**14)
-	PIP_ADAPTER_ADDRESSES bufr;
-	// Let's try without the do loop
-	bufr = (PIP_ADAPTER_ADDRESSES) malloc( adpt_size);
-	// Neva was looking for malloc errors before,
-	// I'm going to skip << out of memory check << or whateva.
-	int r = GetAdaptersAddresses( 
-		AF_UNSPEC,               // both 4 and 6
-		GAA_FLAG_INCLUDE_PREFIX, // not extremly clear
-		nullptr,                 // dummy stuff
-		bufr,
-		&adpt_size);
-	if( r != ERROR_SUCCESS) {
-		std::cout << "NEVA_BEGINZE!\n";
-		au_revoir( 1);
-	}
-	PIP_ADAPTER_ADDRESSES adapta = bufr;
-	while( adapta) {
-		std::cout << "Adapter's Friendly Name: " 
-			      << adapta->FriendlyName << std::endl;
-		IP_ADAPTER_UNICAST_ADDRESS* adr = adapta->FirstUnicastAddress;
-		while( adr) {
-			int family = adr->Address.lpSockaddr->sa_family;
-			if( family == AF_INET) {
-				std::cout << "4\n";
-			} else {
-				std::cout << "6\n";
-			}
-			char rax[ 64];
-			getnameinfo( adr->Address.lpSockaddr,
-				adr->Address.iSockaddrLength,
-				rax, sizeof rax, 0, 0,
-				NI_NUMERICHOST);
-			std::cout << rax << std::endl;
-			adr = adr->Next;
-		}
-		adapta = adapta->Next;
-	}
-	au_revoir( 0);
+////////////////////////////////////////////////////////////////
+# define au_revoir( EXIT_CODE ){                               \
+    delete[] bufr;                                             \
+    WSACleanup();                                              \
+    return EXIT_CODE;                                          \
 }
-// log: Microsoft Visual Studio (Not Responding)
+////////////////////////////////////////////////////////////////
+int GetAdapters( PIP_ADAPTER_ADDRESSES& bufr ){
+    DWORD adapta_size{};
+    bufr = nullptr; // to figure adapta_size
+    for( ;; ){
+        int rslt = GetAdaptersAddresses(
+            AF_UNSPEC,               // both 4 and 6
+            GAA_FLAG_INCLUDE_PREFIX, // wtf's thus?
+            nullptr,                 // dummy stuff( figured! )
+            bufr, 
+            &adapta_size );
+        if( rslt == ERROR_BUFFER_OVERFLOW ){
+            bufr = ( PIP_ADAPTER_ADDRESSES ) new char[ adapta_size ];
+        } else {
+            return rslt;
+        }
+    }
+ }
+////////////////////////////////////////////////////////////////
+int main(){
+    // Initialize winsock library
+    WSADATA wsa_data; // WSA = WinSock API
+    const BYTE MAJOR{ 2 };
+    const BYTE MINOR{ 2 };
+    const WORD VERSION{ MAKEWORD( MAJOR, MINOR )};
+    if( WSAStartup( VERSION, &wsa_data ) != 0 ){
+        std::cout << "WinSock Startup Error.\n";
+        return 1;
+    } else {
+        std::cout << wsa_data << " successfully launched!\n";
+    }
+    PIP_ADAPTER_ADDRESSES bufr{}; 
+    if( GetAdapters( bufr ) != ERROR_SUCCESS ){
+        std::cout << "NEVA_BEGINZE!\n";
+        au_revoir( 1 );
+    }
+    for( auto adapta{ bufr }; adapta; adapta = adapta->Next ){
+        // PWCHAR
+        printf( "\nAdapter name: %s( 0x%llX ): %S", 
+            adapta->AdapterName, 
+            adapta->FriendlyName,
+            adapta->FriendlyName ); 
+        for( auto* addr{ adapta->FirstUnicastAddress }; 
+            addr; addr = addr->Next ){
+            char rax[ 64 ];
+            getnameinfo( 
+                addr->Address.lpSockaddr,
+                addr->Address.iSockaddrLength,
+                rax, sizeof rax, 0, 0,
+                NI_NUMERICHOST );
+            std::cout << endl << rax;
+        }
+    }
+    au_revoir( 0 );
+}
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////// 
