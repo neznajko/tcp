3e3a092c5a9c29f7cd617de810d5dc9e68d04a1f
neznajko <your.email@example.com>
2024-02-06 00:14:21 -0500

    ok

--- Winkle/rust/src/ifaddr_listing.rs
+++ Winkle/rust/src/ifaddr_listing.rs
@@ -4,19 +4,10 @@ use pnet::datalink;
 pub fn ifaddr() {
     let interfaces = datalink::interfaces();
     for interface in interfaces {
-        println!( "{:#?}", interface );
-    }
-}
-//////////////////////////////////////////////////////////////
-/*
-use pnet::datalink::{self, NetworkInterface};
-fn main() {
-    let interfaces = datalink::interfaces();
-    for interface in interfaces {
-        println!("Interface: {}", interface.name);
-        for ip_network in &interface.ips {
-            println!("  IP Address: {}", ip_network.ip());
+        println!( "Interface: {}", interface.name );
+        for ip in interface.ips {
+            println!( " IP Address: {}", ip.ip() );
         }
     }
-*/
+}
 //////////////////////////////////////////////////////////////
